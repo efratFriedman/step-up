213b34822c8687304a347c31dd767ac7f350bd46
Mindi1234 <mimikenig1@gmail.com>
2025-11-09 12:27:27 +0200

    finish signup

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,6 @@
+// import { NextResponse } from "next/server";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import User from '@/models/UserSchema'
+// import connectDB from "@/lib/mongodb";
+
--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
-import { User } from "@/types/user";
+import { dbConnect } from "@/lib/DB";
+import User from "@/models/User";
 import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
 
 
 export async function POST(request: Request) {
   try {
+    await dbConnect();
     const { name, password, birthDate, phone, email } = await request.json();
 
     console.log("New user:", { name, password, birthDate, phone, email });
@@ -16,12 +18,48 @@ export async function POST(request: Request) {
         { status: 400 }
       );
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "this email is already exist!" },
+        { status: 400 }
+      );
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
-    return NextResponse.json({
+    const newUser = new User({
+      name,
+      email,
+      phone,
+      birthDate,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    const token = jwt.sign(
+      { id: newUser._id, email: newUser.email },
+      process.env.JWT_SECRET!,
+      { expiresIn: "7d" } 
+    );
+
+    const response = NextResponse.json({
       message: `Welcome ${name}! Your account has been created.`,
     });
+   
+    response.cookies.set("token", token, {
+      httpOnly: true, 
+      secure: process.env.NODE_ENV === "production",
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7, 
+    });
+
+    return response;
   } catch (error) {
     console.error("Signup error:", error);
-    return NextResponse.json({ message: "Something went wrong" }, { status: 500 });
+    return NextResponse.json(
+      { message: "error!" },
+      { status: 500 }
+    );
   }
 }
