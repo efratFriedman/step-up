a974e13b20409758a72829d5eb6c05755e137782
Efrat Friedman <efratfriedman18@gmail.com>
2025-11-09 11:33:24 +0200

    change the export syntax of the schema

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,12 +1,12 @@
 import { IPost } from '@/interfaces/IPost';
 import mongoose, { Schema, Model, model } from 'mongoose';
 
-const PostSchema: Schema<IPost> = new Schema({
+const PostSchema = new mongoose.Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   content: String,
   media: [String], 
   likesCount: { type: Number, default: 0 }
 });
 
-const Post: Model<IPost> = model<IPost>('Post', PostSchema);
-export default Post;
+export default mongoose.models.Post||mongoose.model<IPost>("Post",PostSchema);
+
