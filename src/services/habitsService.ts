39d2c960d8cc8a604af1da14165fa3341a515abb
Efrat Friedman <efratfriedman18@gmail.com>
2025-11-18 11:12:12 +0200

    fix and change to service the fetch calls

--- src/services/habitsService.ts
+++ src/services/habitsService.ts
@@ -1,9 +1,27 @@
 import { IHabit } from "@/interfaces/IHabit";
 
-export async function getUserHabits(userId:string):Promise<IHabit[]>{
-    const res=await fetch(`/api/habits?userId=${userId}`,
-        {method:"GET",
-
+export async function getUserHabits(userId: string): Promise<IHabit[]> {
+    const res = await fetch(`/api/habits?userId=${userId}`,
+        {
+            method: "GET",
         });
-        return res.json();
+    return res.json();
+}
+
+export async function getTodayHabits():Promise<IHabit[]> {
+    const res = await fetch('/api/habits/today',
+        {
+            method:"GET",
+        }
+    );
+    return res.json();
+
+}
+
+export async function updateHabitStatus(habitId:string){
+    const res=await fetch(`/api/habits/toggle/${habitId}`,
+        {method:"POST"},
+
+    )
+    return res.json();
 }
