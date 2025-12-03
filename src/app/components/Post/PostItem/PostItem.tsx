624a22ff815ded783b5bec838d39c268b600db6d
Mindi1234 <mimikenig1@gmail.com>
2025-12-03 10:05:29 +0200

    check likes

--- src/app/components/Post/PostItem/PostItem.tsx
+++ src/app/components/Post/PostItem/PostItem.tsx
@@ -1,32 +1,61 @@
+"use client";
+
+import { useState } from "react";
+import { toggleLike } from "@/services/client/postService";
 import { IPost } from "@/interfaces/IPost";
 import Slider from "../Slider/Slider"; 
 import styles from "./PostItem.module.css";
+import { Heart } from "lucide-react";
 
 export default function PostItem({ post }: { post: IPost & { userId: { name: string; profileImg?: string } } }) {
+  const [likes, setLikes] = useState(post.likesCount);
+  const [liked, setLiked] = useState(
+    post.likedBy?.includes(post.currentUserId ?? "") || false
+  );
+
+  const onLike = async () => {
+    try {
+      const res = await toggleLike(post._id);
+      setLikes(res.likesCount);
+      setLiked(res.liked);
+    } catch (error) {
+      console.error("Error toggling like:", error);
+    }
+  }
 
   return (
     <div className={styles.postItem}>
-    <div className={styles.profile}>
-      <img 
-        src={post.userId.profileImg || "/default-profile.png"} 
-        alt={post.userId.name} 
-        className={styles.profileImg} 
-      />
-      <p className={styles.userName}>{post.userId.name}</p>
-    </div>
-
-    <div className={styles.content}>
-
-      {post.media && post.media.length > 0 && (
-        <Slider 
-          items={post.media.map((item) => ({
-            url: item.url,
-            type: item.type, 
-          }))} 
+      <div className={styles.profile}>
+        <img 
+          src={post.userId.profileImg || "/default-profile.png"} 
+          alt={post.userId.name} 
+          className={styles.profileImg} 
         />
-      )}
-      <p className={styles.postText}>{post.content}</p>
+        <p className={styles.userName}>{post.userId.name}</p>
+      </div>
+
+      <div className={styles.content}>
+
+        {/* תמונות/וידאו */}
+        {post.media && post.media.length > 0 && (
+          <Slider items={post.media.map((item) => ({
+            url: item.url,
+            type: item.type,
+          }))} />
+        )}
+
+        <p className={styles.postText}>{post.content}</p>
+
+        <div className={styles.likeWrapper} onClick={onLike}>
+          <Heart 
+            fill={liked ? "red" : "transparent"} 
+            color={liked ? "red" : "black"}
+            className={styles.likeIcon}
+          />
+          <span className={styles.likesCount}>{likes}</span>
+        </div>
+
+      </div>
     </div>
-  </div>
   );
 }
